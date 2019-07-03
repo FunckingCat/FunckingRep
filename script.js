e3875990273988bb7a923b4138672b130fe93536
David Zh <david99111@mail.ru>
2019-07-03 17:12:17 +0300

    Finished MYCOMPANY Layout

---
+++ script.js
@@ -0,0 +1,7 @@
+width=screen.width; // ширина
+height=screen.height; // высота
+//alert ("Разрешение экрана: "+width+"x"+height);
+
+width=document.body.clientWidth; // ширина
+height=document.body.clientHeight; // высота
+//alert ("Разрешение окна клиента: "+width+"x"+height);
